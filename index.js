7a6887b2b0549adc3ab61deca0288f627c2825d4
Admin <jayeshkhadke4309@gmail.com>
2024-08-25 12:12:25 +0530

    Update Express server to handle /bfhl endpoint

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app
 
     res.json({
       is_success: true,
-      user_id: "sanhita17",
-      email: "sanhita.kundu2020@vitstudent.ac.in",
-      roll_number: "20BEC0215",
+      user_id: "jayeshk7",
+      email: "jayeshjayant.khadke2021@vitstudent.ac.in",
+      roll_number: "21BCE1781",
       numbers: numbers,
       alphabets: alphabets,
       highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
